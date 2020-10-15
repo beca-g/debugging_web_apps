9306395bb259a6a8b88f9df1cc14dbf4854e8141
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-15 21:20:25 +0100

    Corrected tests in random_happy_emoji unit tests

--- spec/units/random_happy_emoji_spec.rb
+++ spec/units/random_happy_emoji_spec.rb
@@ -5,13 +5,13 @@ require 'random_happy_emoji'
 describe RandomHappyEmoji do
   describe '::new' do
     it 'returns a :D emoji as a string', focus: true do
-      srand(67810)
-      expect(RandomHappyEmoji.new.to_s).to eq ":)"
+      srand(67809)
+      expect(RandomHappyEmoji.new.to_s).to eq ":D"
     end
 
     it 'returns a :) emoji as a string', focus: true do
-      srand(67809)
-      expect(RandomHappyEmoji.new.to_s).to eq ":D"
+      srand(67810)
+      expect(RandomHappyEmoji.new.to_s).to eq ":)"
     end
   end
 end
